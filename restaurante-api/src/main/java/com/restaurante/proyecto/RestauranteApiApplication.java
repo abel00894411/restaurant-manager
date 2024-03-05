e2ed2a8f7d932aa34afae2e78bb60614e187fea7
Abel <pintorgarciaabel4142@gmail.com>
2024-03-04 20:17:58 -0600

    Creación del proyecto y de los directorios base para el comienzo del proyecto
    Signed-off-by: Abel <pintorgarciaabel4142@gmail.com>

---
+++ restaurante-api/src/main/java/com/restaurante/proyecto/RestauranteApiApplication.java
@@ -0,0 +1,13 @@
+package com.restaurante.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestauranteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestauranteApiApplication.class, args);
+	}
+
+}
---
+++ restaurante-api/src/test/java/com/restaurante/proyecto/RestauranteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurante.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestauranteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
