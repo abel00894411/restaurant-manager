712486b5e46d450160464a5ba5975416c81187f1
Gerardo Arzate <116304288+gerardoarzate55@users.noreply.github.com>
2024-04-24 14:34:09 -0600

    Agregar rutas publicas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,15 @@ import { Outlet } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import './App.css';
 
-function App() {
+function App({ token }) {
+    const className = token ? 'employee-layout' : 'public-layout';
+    const content = token ? <> <Navigation /> <Outlet /> </> : <Outlet />;
+
     return (
-        <div className="layout-container">
-            <Navigation />
-            <Outlet />
+        <div className={className} >
+            { content }
         </div>
-    )
+    );
 }
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, redirect, RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
 import * as Waiter from './routes/waiter/index.jsx';
 import * as Cook from './routes/cook/index.jsx';
 import * as Admin from './routes/admin/index.jsx';
+import * as Customer from './routes/customer/index.jsx';
 import logout from './util/logout.js';
 import getTokenData from './util/getTokenData.js';
 
@@ -41,22 +42,41 @@ const adminPaths = [
     }
 ];
 
-const token = getTokenData() || logout();
+const customerPaths = [
+    {
+        path: '/',
+        element: <h1>Public index</h1>
+    },
+    {
+        path: '/menu',
+        element: <Customer.MenuPage />
+    }
+];
+
+let token = getTokenData();
 
 // Choose the correct path list for the type of user logged-in
-const paths = (
-    token.tipo == 'MESERO' ? waiterPaths
-    : token.tipo == 'COCINERO' ? cookPaths
-    : token.tipo == 'ADMINISTRADOR' ? adminPaths
-    : logout()
+let paths = (
+    token?.tipo == 'MESERO' ? waiterPaths
+    : token?.tipo == 'COCINERO' ? cookPaths
+    : token?.tipo == 'ADMINISTRADOR' ? adminPaths
+    : undefined
 );
 
-const router = createBrowserRouter([{
-    path: '/',
-    element: <App />,
-    errorElement: <h1>Error de ruta</h1>,
-    children: paths
-}]);
+if (!paths) {
+    logout({ redirect: false });
+    token = getTokenData();
+    paths = customerPaths
+}
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App token={getTokenData()} />,
+        errorElement: <h1>Error de ruta</h1>,
+        children: paths
+    }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
