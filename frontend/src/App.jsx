531777a50c4921570f0926c8f99517d68e987c82
Gerardo Arzate <116304288+gerardoarzate55@users.noreply.github.com>
2024-04-24 16:15:08 -0600

    Actualizar barra de navegación

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,9 @@ import { Outlet } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import './App.css';
 
-function App({ token }) {
+function App({ token, paths }) {
     const className = token ? 'employee-layout' : 'public-layout';
-    const content = token ? <> <Navigation /> <Outlet /> </> : <Outlet />;
+    const content = token ? <> <Navigation paths={paths} /> <Outlet /> </> : <Outlet />;
 
     return (
         <div className={className} >
--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,9 +1,45 @@
+import { useState } from 'react';
+import { useLocation, Link } from 'react-router-dom';
 import './Navigation.css';
 
-const Navigation = () => {
+const Navigation = ({ paths }) => {
+    let options = [];
+
+    for (const path of paths) {
+        const option = options.find(opt => path.display == opt.display);
+
+        if (!option) {
+            path.equivalentPaths = [ path.path ];
+            options.push(path);
+        } else {
+            option.equivalentPaths.push(path.path);
+        }
+    }
+
+    const optClassName = 'navigation__options__option';
+    const currentOptClassName = optClassName + '--current';
+
+    const location = useLocation().pathname;
+
     return (
         <nav className="navigation">
-            Navigation
+            <div className="navigation__logo-container">
+                <div className="navigation__logo-container__logo"></div>
+            </div>
+
+            <nav className="navigation__options">
+                {
+                    options.map((opt, i) =>
+                        <Link 
+                            key={i}
+                            className={opt.equivalentPaths.includes(location) ? currentOptClassName : optClassName}
+                            to={opt.path}
+                        >
+                            {opt.display}
+                        </Link>
+                    )
+                }
+            </nav>
         </nav>
     );
 }
