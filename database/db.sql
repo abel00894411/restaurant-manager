bccead11ad10a05ea2e6770b13c76d5add84922d
Abel <pintorgarciaabel4142@gmail.com>
2024-05-06 12:42:22 -0600

    Agregar tabla empleado eliminada por error

--- database/db.sql
+++ database/db.sql
@@ -1,5 +1,17 @@
 CREATE DATABASE restaurante_analisis;
 
+CREATE TABLE `restaurante_analisis`.`empleados` (
+  `idEmpleado` INT NOT NULL AUTO_INCREMENT,
+  `nombre` VARCHAR(50) NOT NULL,
+  `apellido` VARCHAR(50) NOT NULL,
+  `password` VARCHAR(100) NOT NULL,
+  `rfc` VARCHAR(13) NOT NULL UNIQUE,
+  `sueldo` DECIMAL(10,2) NOT NULL,
+  `fechaContratacion` DATE NOT NULL,
+  PRIMARY KEY (`idEmpleado`)
+);
+
+
 CREATE TABLE `restaurante_analisis`.`administradores` (
   `idAdministrador` INT NOT NULL,
   PRIMARY KEY (`idAdministrador`),
