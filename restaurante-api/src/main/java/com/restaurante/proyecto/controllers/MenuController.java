97f0e9ee788b6d3e201ea70fefb94e9262addeb9
Abel <pintorgarciaabel4142@gmail.com>
2024-05-12 22:13:40 -0600

    Crear controladores de Menu y de Orden, agregar clase utilitaria para manipular imagenes y DTO's para la transferencia de informacion

--- restaurante-api/src/main/java/com/restaurante/proyecto/controllers/MenuController.java
+++ restaurante-api/src/main/java/com/restaurante/proyecto/controllers/MenuController.java
@@ -1,12 +1,75 @@
 package com.restaurante.proyecto.controllers;
 
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/menu") // aqui si todas los controladores de esta clase ya por default tienen la ruta '/menu'
-// esto no se hizo en UsuarioController porque no todos son '/usuarios' hay algunos que son '/usuario'
+import com.restaurante.proyecto.dtos.ItemMenuCrear;
+import com.restaurante.proyecto.dtos.ItemMenuResponse;
+import com.restaurante.proyecto.models.entity.ItemMenu;
+import com.restaurante.proyecto.services.MenuService;
+import com.restaurante.proyecto.utils.ImageHelper;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+@RestController
 public class MenuController {
 	
+	@Autowired
+	private MenuService menuService;
 	
 	
+	@GetMapping("/menu")
+	public ResponseEntity<?> obtenerMenu() {
+//		 HttpHeaders headers = new HttpHeaders();
+//		 headers.setContentType(MediaType.IMAGE_JPEG); 
+//		return new ResponseEntity<>(imagen, headers, HttpStatus.OK);
+		List<ItemMenuResponse> itemsMenuResponse = menuService.obtenerMenu();
+		Map<String, Object> response = new HashMap<String, Object>();
+		
+		response.put("itemsMenu", itemsMenuResponse);
+		return ResponseEntity.ok(response);
+	}
+	
+	@PostMapping("/menu")
+	public ResponseEntity<?> crearItemMenu(@RequestBody ItemMenuCrear itemMenuCrear) {
+		ItemMenu itemMenu = menuService.crearItemMenu(itemMenuCrear);
+		Map<String, Object> response = new HashMap<String, Object>();
+		response.put("itemMenu", itemMenu);
+		return ResponseEntity.ok("simon");
+	}
+	
+	
+	@PutMapping("/menu/{id}")
+	public ResponseEntity<?> actualizar(@PathVariable Long id, @RequestBody ItemMenuCrear itemMenuActualizar) {
+		
+		menuService.actualizarItemMenu(id,itemMenuActualizar);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
+	
+	
+	@DeleteMapping("/menu/{id}")
+	public ResponseEntity<?> eliminar(@PathVariable Long id){
+		
+		menuService.eliminarItemMenu(id);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
 	
 }
