f2ab00c01f8c9e0b242069dd0b5ba13f6ac96bb1
Gerardo Arzate <116304288+gerardoarzate55@users.noreply.github.com>
2024-05-19 23:33:16 -0600

    Actualizar Menu

--- frontend/src/models/Menu.ts
+++ frontend/src/models/Menu.ts
@@ -6,34 +6,45 @@ import devLog from '../util/devLog';
  * When instanciated, fetches menu data from backend and stores it.
  */
 class Menu {
-    // #categories: object[] = [];
-    #categories: object[] = [ {id: 1, categoria: 'postres'} ]; // Remove this
+    #categories: object[] = [];
     #items: MenuItem[] = [];
     #constructed = false;
 
-
-    // TODO: obtener las categorias del backend
     constructor() {
-        fetchAPI('menu', 'GET')
-            .then((res: any) => {
-                res.itemsMenu.forEach((item: any) => {
-                    const categoryObj: any = this.#categories.find((cat: any) => cat.id == item.categoria);
-
-                    if (!categoryObj) {
-                        throw new Error(`undefined category for item ${item.producto} (category id ${item.categoria})`);
-                    }
-
-                    const categoryName = categoryObj.categoria;
-
-                    const menuItem = new MenuItem(item.idItemMenu, item.producto, categoryName, item.precio, item.descripcion, item.imagen);
-                    this.#items.push(menuItem);
+        fetchAPI('categorias', 'GET')
+            .then(async (resCat: any) => {
+                
+                resCat.categorias.forEach((cat: any) => {
+                    this.#categories.push(cat);
                 });
 
-                this.#constructed = true;
+                try {
+                    const resMenu: any = await fetchAPI('menu', 'GET');
+
+                    resMenu.itemsMenu.forEach((item: any) => {
+                        const categoryObj: any = this.#categories.find((cat: any) => cat.idCategoria == item.categoria);
+    
+                        if (!categoryObj) {
+                            throw new Error(`undefined category for item ${item.producto} (category id ${item.categoria})`);
+                        }
+    
+                        const categoryName = categoryObj.categoria;
+    
+                        const menuItem = new MenuItem(item.idItemMenu, item.producto, categoryName, item.precio, item.descripcion, item.imagen);
+                        this.#items.push(menuItem);
+                    });
+    
+                    this.#constructed = true;
+
+                } catch(error) {
+                    devLog(`Unable to instanciate Menu object: ${error.message}`);
+                    alert('No se pudo obtener los datos del menú, vuelve a intentarlo.');
+                }
+                    
             })
             .catch(error => {
-                devLog(`Unable to instanciate Menu object: ${error.message}`);
-                alert('No se pudo obtener los datos del menú, vuelve a intentarlo.');
+                devLog(`Unable to get categories: ${error.message}`);
+                alert(`No se pudo obtener los datos de las categorías del menú, vuelve a intentarlo.`);
             })
     }
 
