5c8d44bc674b798542fb329e3d32efbf21959b58
Gerardo Arzate <116304288+gerardoarzate55@users.noreply.github.com>
2024-05-24 13:32:19 -0600

    Agregar formulario de edición de usuario para administrador

---
+++ frontend/src/routes/admin/EditUserPage.jsx
@@ -0,0 +1,24 @@
+import { useLocation } from "react-router-dom";
+import UserForm from "../../components/UserForm";
+import getTokenData from "../../util/getTokenData";
+
+const EditUserPage = () => {
+    const url = useLocation().pathname;
+    const userId = url.split('/').reverse()[0];
+
+    const exclude = [];
+
+    if (getTokenData().idUsuario != userId) {
+        exclude.push('password');
+    }
+
+    return (
+        <>
+            <h1>Modificar usuario</h1>
+
+            <UserForm userId={userId} exclude={exclude}/>
+        </>
+    );
+};
+
+export default EditUserPage;
