3fd43361d871049ea5cea27b3dc354267eafc803
Gerardo Arzate <116304288+gerardoarzate55@users.noreply.github.com>
2024-05-24 10:54:34 -0600

    Agregar botón para nuevo usuario en ruta usuarios de administrador

--- frontend/src/routes/admin/UsersPage.jsx
+++ frontend/src/routes/admin/UsersPage.jsx
@@ -1,10 +1,12 @@
 import AdminUserTable from "../../components/AdminUserTable";
+import AddButton from "../../components/AddButton";
 
 const UsersPage = () => {
     return (
         <>
             <h1>Ver usuarios</h1>
             <AdminUserTable />
+            <AddButton redirect="/usuarios/nuevo">Nuevo usuario</AddButton>
         </>
     );
 };
