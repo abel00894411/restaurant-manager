b605b57506718bc5a10089390f5b9af2cb7606c1
Gerardo Arzate <116304288+gerardoarzate55@users.noreply.github.com>
2024-05-25 13:28:39 -0600

    Agregar ruta de perfil a usuarios cocineros y meseros

--- frontend/src/routes/waiter/ProfilePage.jsx
+++ frontend/src/routes/waiter/ProfilePage.jsx
@@ -1,5 +1,17 @@
+import UserForm from "../../components/UserForm";
+import getTokenData from '../../util/getTokenData';
+
+const token = getTokenData();
+const userId = token.idUsuario;
+
 const ProfilePage = () => {
-    return <>Profile page</>;
-};
+    return (
+        <>
+            <h1>Configuración de usuario</h1>
+
+            <UserForm userId={userId} passwordOnly={true} />
+        </>
+    );
+}
 
 export default ProfilePage;
