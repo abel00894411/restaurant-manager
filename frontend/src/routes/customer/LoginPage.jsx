423542c861ffbe572f92c2f88b032b35d962aabb
FrancisK7L <fco.lunafdez7448123@gmail.com>
2024-05-24 22:26:22 -0600

    Agregar cambios

--- frontend/src/routes/customer/LoginPage.jsx
+++ frontend/src/routes/customer/LoginPage.jsx
@@ -1,9 +1,35 @@
-const LoginPage = () => {
-    return (
-        <main>
-            Login Page
-        </main>
-    );
-};
+import React from 'react';
+import './Login.css'; 
 
-export default LoginPage;
+function Login() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>[ LOGO ]</h1>
+      </header>
+      <main>
+        <h2>¡Bienvenido a nuestro restaurante!</h2>
+        <div className="login-box">
+          <h3>Empleado, inicia sesión</h3>
+          <form>
+            <label>
+              RFC
+              <input type="text" name="rfc" />
+            </label>
+            <label>
+              Contraseña
+              <input type="password" name="password" />
+            </label>
+            <button type="submit">Continuar</button>
+          </form>
+        </div>
+        <div className="clientes">
+          <a href="/facturas">Genera tus facturas &gt;</a>
+          <a href="/menu">Mira nuestro menú &gt;</a>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default Login;
