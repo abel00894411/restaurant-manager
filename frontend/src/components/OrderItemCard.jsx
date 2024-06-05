d2b7929de9211ef002feb27cbb126364c4b844e4
Gerardo Arzate <116304288+gerardoarzate55@users.noreply.github.com>
2024-06-05 11:30:14 -0600

    Actualizar OrderItemCard, etiqueta de tiempo cambia de color con el paso del tiempo y es responsiva

--- frontend/src/components/OrderItemCard.jsx
+++ frontend/src/components/OrderItemCard.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import OrderItem from '../models/OrderItem';
 import SmallButton from './SmallButton';
 import { menu } from '../models/Menu';
@@ -13,9 +13,6 @@ const OrderItemCard = ({ orderItem, onClick = ()=>{}, inPreparation = false }) =
     if (!(orderItem instanceof OrderItem)) {
         throw new Error(`Unable to instanciate OrderItemCard component: ${orderItem} is not an instance of OrderItem`);
     }
-
-    // TODO: Update the color of the time text as the item gets older
-    // TODO: Make the time text responsive, show only time in smaller screen sizes
     
     const resizeBreakpoint = 500;
 
@@ -23,16 +20,45 @@ const OrderItemCard = ({ orderItem, onClick = ()=>{}, inPreparation = false }) =
     const buttonBigSize = 64;
     const initialInPreparationButtonSize = window.innerWidth < resizeBreakpoint ? buttonSmallSize : buttonBigSize;
 
+    const timeLabelColorDuration = 60 * 10; // In seconds
+    const timeLabelColorUpdateIntervalDelay = 10; // In seconds
+
     const [ buttonSize, setButtonSize ] = useState(inPreparation ? initialInPreparationButtonSize : buttonSmallSize);
+    const [ timeLabelCompact, setTimeLabelCompact ] = useState(window.innerWidth < resizeBreakpoint);
+    const [ timeLabelColor, setTimeLabelColor ] = useState('var(--gray)');
 
     const onResize = (event) => {
-        let newSize = (window.innerWidth < resizeBreakpoint) ? buttonSmallSize : buttonBigSize;
+        const onSmallScreen = window.innerWidth < resizeBreakpoint;
+        let newSize = onSmallScreen ? buttonSmallSize : buttonBigSize;
         setButtonSize(newSize);
+        setTimeLabelCompact(onSmallScreen);
+    };
+
+    const updateTimeLabelColor = () => {
+        const cardLifetime = (Date.now() - date.getTime()) / 1000; // In seconds
+        let color = 'var(--gray)';
+        
+        if (cardLifetime > timeLabelColorDuration) {
+            color = '#ECC706'; // Yellow
+        }
+
+        if (cardLifetime > timeLabelColorDuration*2) {
+            color = '#c0392b'; // Red
+        }
+
+        setTimeLabelColor(color);
     };
 
     if (inPreparation) {
         useEventListener('resize', onResize, window);
     }
+
+    useEffect(() => {
+        const timeLabelColorUpdateInterval = setInterval(updateTimeLabelColor, timeLabelColorUpdateIntervalDelay * 1000);
+
+        return () => clearInterval(timeLabelColorUpdateInterval)
+    }, []);
+
 
     const parentClass = inPreparation ? 'orderItemCard--in-preparation' : 'orderItemCard';
 
@@ -57,9 +83,13 @@ const OrderItemCard = ({ orderItem, onClick = ()=>{}, inPreparation = false }) =
                     { inPreparation && <span className="material-symbols-outlined">skillet</span> }
                 </p>
 
-                <p className={`${parentClass}__details__time`}>
+                <p className={`${parentClass}__details__time`} style={{ color: timeLabelColor }}>
                     <span className="material-symbols-outlined">schedule</span>
-                    Ordenado a las {formattedTime}
+                    { (timeLabelCompact || !inPreparation) ?
+                        <>{formattedTime}</>
+                        :
+                        <>Ordenado a las {formattedTime}</>
+                    }
                 </p>
             </div>
 
