ed75760790027494d0e23cbd647c241f096eb5ad
Gerardo Arzate <116304288+gerardoarzate55@users.noreply.github.com>
2024-06-06 15:28:30 -0600

    Actualizar ruta de facturación para clientes

--- frontend/src/routes/customer/InvoicePage.jsx
+++ frontend/src/routes/customer/InvoicePage.jsx
@@ -1,149 +1,154 @@
-import React from 'react';
 import fetchAPI from '../../util/fetchAPI';
 import { useState } from 'react';
 import devLog from '../../util/devLog';
 import { menu } from '../../models/Menu';
+import { Link } from 'react-router-dom';
 import './InvoicePage.css';
+import generateInvoicePDF from '../../util/generateInvoicePDF';
 
-const currencyFormatter = new Intl.NumberFormat('es-MX', { style: 'currency', currency: 'MXN', maximumFractionDigits: 0 });
+const currencyFormatter = new Intl.NumberFormat('es-MX', { style: 'currency', currency: 'MXN', maximumFractionDigits: 2 });
 
 const InvoicePage = () => {
-    const [invoiceData, setInvoiceData] = useState({
-        idOrden: '',
-        correo: '',
-        direccion: '',
-        rfc: ''
+    const [ formData, setFormData ] = useState({
+        rfc: '',
+        email: '',
+        address: '',
+        orderId: ''
     });
 
-    const [orderData, setOrderData] = useState(undefined);
-
-    const submitInvoice = (invoiceData) => {
-        
-        fetchAPI('/facturas', 'POST', invoiceData)
-            .then(res => {
-                
-                setOrderData(res.factura.orden);
-
-            })
-            .catch(error => {
-                alert(`No fue posible generar la factura`);
-                devLog(error.message);
-            });
-    };
+    const [ invoiceData, setInvoiceData ] = useState(undefined);
 
     const handleChange = (event) => {
-        const { target } = event;
+        if (invoiceData) {
+            return;
+        }
 
-        setInvoiceData(old => {
+        const input = event.target;
+
+        setFormData(oldData => {
             return {
-                ...old,
-                [target.name]: target.value
+                ...formData,
+                [input.name]: input.value
             };
         });
     };
 
-    const generateInvoice = (orderData) => {
-        let res = '<h1>Factura</h1>';
+    const handleGenerateClick = () => {
+        const { rfc, email, address, orderId } = formData;
 
-        for (const field in orderData) {
-            const ffield = `${field[0].toUpperCase()}${field.slice(1).toUpperCase()}`;
-            if (field == 'items') {
-                for (const item of (orderData.items)) {
-                    res += `<p>${menu.getItem(item.idItemMenu).name}</p>`;
-                    res += `<ol class='invoice-item'>`;
-                        res += `<li>Precio unitario: ${currencyFormatter.format(menu.getItem(item.idItemMenu).price)}</li>`;
-                        res += `<li>Precio total: ${currencyFormatter.format(menu.getItem(item.idItemMenu).price*item.cantidad)}</li>`;
-                    res += `</ol>`;
-                }
-
-                continue;
-            }
-
-            res += `<p>${ffield}: ${orderData[field]}</p>`;
+        if ( !rfc || !email || !address || !orderId ) {
+            alert(`Por favor, llena todos los campos`);
+            return;
         }
 
-        document.body.innerHTML = res;
-        print();
+        fetchAPI('facturas', 'POST', {
+            idOrden: orderId,
+            correo: email,
+            direccion: address,
+            rfc: rfc
+        })
+        .then(res => {
+            setInvoiceData(res.factura);
+            generateInvoicePDF(res.factura);
+        })
+        .catch(error => {
+            alert('No fue posible generar la factura');
+            devLog(error.message);
+        });
     };
 
     return (
-        <div className="invoice-form-container">
-            <header className="invoice-form-header">
-                <img src="/img/logo.png" alt="Logotipo" />
-            </header>
-            <div className='form-main'>
-                <h2>Genera tu factura</h2>
-                <div className="form-box">
-                    <h3>Por favor, introduce tus datos</h3>
-                    <form className='invoice-form'>
-                        <label>
-                            RFC
-                            <input autoComplete='off' type="text" name="rfc" value={invoiceData.rfc} onChange={handleChange} />
-                        </label>
-                        <label>
-                            Correo electrónico
-                            <input autoComplete='off' type="email" name="correo" value={invoiceData.correo} onChange={handleChange} />
-                        </label>
-                        <label>
-                            Dirección de facturación
-                            <input autoComplete='off' type="text" name="direccion" value={invoiceData.direccion} onChange={handleChange} />
-                            
-                        </label>
-                        <label>
-                            Identificador de orden
-                            <input autoComplete='off' type="text" name="idOrden" value={invoiceData.idOrden} onChange={handleChange} />
-                        </label>
-                        <button type="button" onClick={()=>submitInvoice(invoiceData)}>Buscar orden</button>
-                    </form>
-
-                    { orderData && 
-                        <>
-                            <table>
-                                <thead>
-                                    <tr>
-                                        <th>Items</th>
-                                        <th>Precio unitario</th>
-                                        <th>Cantidad</th>
-                                        <th>Precio total</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    { orderData.items.map((item, i) => {
-                                        return (
-                                            <tr key={i}>
-                                                <td>{item.producto}</td>
-                                                <td>{currencyFormatter.format(menu.getItem(item.idItemMenu).price)}</td>
-                                                <td>{item.cantidad}</td>
-                                                <td>{currencyFormatter.format(menu.getItem(item.idItemMenu).price*item.cantidad)}</td>
-                                            </tr>
-                                        );
-                                    }) }
-                                </tbody>
-                                <tfoot>
-                                    <tr>
-                                        <td></td><td></td>
-                                        <td>Subtotal</td>
-                                        <td>{orderData.subtotal}</td>
-                                    </tr>
-                                    <tr>
-                                        <td></td><td></td>
-                                        <td>IVA (16%)</td>
-                                        <td>{orderData.iva}</td>
-                                    </tr>
-                                    <tr>
-                                        <td></td><td></td>
-                                        <td>Total</td>
-                                        <td>{orderData.total}</td>
-                                    </tr>
-                                </tfoot>
-                            </table>
-
-                            <button onClick={generateInvoice(orderData)}>Generar factura</button>
-                        </>
-                    }
-                </div>
+        <>
+            <div className="invoicePage__image-container">
+                <Link to="/">
+                    <img src="/img/logo.png" alt="Logotipo" />
+                </Link>
             </div>
-        </div>
+
+            <h1 className='invoicePage__title'>Genera tu factura</h1>
+
+            <form className="invoicePage__form thin-border">
+                <h4>Por favor, introduce tus datos</h4>
+
+                <label>
+                    RFC
+                    <input type="text" name="rfc" onChange={handleChange} autoComplete='off' />
+                </label>
+
+                <label>
+                    Correo electrónico
+                    <input type="text" name="email" onChange={handleChange} autoComplete='off' />
+                </label>
+
+                <label>
+                    Dirección de facturación
+                    <input type="text" name="address" onChange={handleChange} autoComplete='off' />
+                </label>
+
+                <label>
+                    Identificador de orden
+                    <input type="text" name="orderId" onChange={handleChange} autoComplete='off' />
+                </label>
+
+                {
+                    !invoiceData &&
+                    <button type="button" onClick={handleGenerateClick}>Generar factura</button>
+                }
+
+                { invoiceData &&
+                    <>
+                        <h4>Orden #{formData.orderId}</h4>
+
+                        <table className='invoicePage__table'>
+                            <thead>
+                                <tr>
+                                    <th>Ítems</th>
+                                    <th>Precio unitario</th>
+                                    <th>Cantidad</th>
+                                    <th>Precio total</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                { invoiceData.orden.items.map((item, i) => {
+                                    const menuItem = menu.getItem(item.idItemMenu);
+                                    const itemName = menuItem?.name;
+                                    const itemPrice = menuItem?.price || 0;
+                                    const quantity = item.cantidad;
+                                    const totalItemPrice = itemPrice * quantity;
+
+                                    return (
+                                        <tr key={i}>
+                                            <td>{itemName}</td>
+                                            <td>{currencyFormatter.format(itemPrice)}</td>
+                                            <td>{quantity}</td>
+                                            <td>{currencyFormatter.format(totalItemPrice)}</td>
+                                        </tr>
+                                    )}
+                                ) }
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <td colSpan={3} >Subtotal</td>
+                                    <td>{ currencyFormatter.format(invoiceData.orden.subtotal) }</td>
+                                </tr>
+                                <tr>
+                                    <td colSpan={3} >IVA (16%)</td>
+                                    <td>{ currencyFormatter.format(invoiceData.orden.iva) }</td>
+                                </tr>
+                                <tr>
+                                    <td colSpan={3} >Total</td>
+                                    <td>{ currencyFormatter.format(invoiceData.total) }</td>
+                                </tr>
+                            </tfoot>
+                        </table>
+                        
+                        <button type="button" onClick={() => generateInvoicePDF(invoiceData)}>Obtener PDF</button>
+                    </>
+                }
+            </form>
+
+        </>
+        
     );
 };
 
